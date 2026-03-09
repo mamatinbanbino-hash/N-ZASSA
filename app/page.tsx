303a3e4e3c0b0bf2a8a6da080be36eea5cbdbe3c
mamatinbanbino-hash <mamatinbanbino@gmail.com>
2026-03-09 01:57:26 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,18 @@ export default function Home() {
       flexDirection: 'column', 
       alignItems: 'center', 
       justifyContent: 'center', 
-      backgroundColor: 'black' 
+      textAlign: 'center'
     }}>
       <h1 style={{ fontSize: '60px', color: '#4ade80', margin: '0' }}>N'ZASSA</h1>
       <p style={{ color: '#71717a', fontSize: '20px' }}>Le réseau de Macassi est EN LIGNE !</p>
-      <div style={{ marginTop: '20px', padding: '15px', border: '1px solid #27272a', borderRadius: '10px' }}>
-        <p style={{ color: '#4ade80' }}>● Serveur : Opérationnel</p>
-        <p>Propriétaire : N’diaye Adama</p>
+      <div style={{ 
+        marginTop: '20px', 
+        padding: '20px', 
+        border: '1px solid #27272a', 
+        borderRadius: '12px' 
+      }}>
+        <p style={{ color: '#4ade80', fontWeight: 'bold' }}>● Statut : Opérationnel</p>
+        <p>Développeur : N’diaye Adama</p>
       </div>
     </div>
   )
