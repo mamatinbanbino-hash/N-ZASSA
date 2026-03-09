8a7491f17adf2a652240fae1be56095ed4bd9cdf
mamatinbanbino-hash <mamatinbanbino@gmail.com>
2026-03-09 01:40:32 +0000

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: "N'ZASSA",
+  description: 'Le réseau de Macassi',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="fr">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
